4909465f513640ee4851fb6fe8bc63b37a1413ae
Prashant Khurana <prashant@prashantkhurana.com>
2017-06-25 23:30:03 -0400

    tsx more

--- src/components/Bidders/AllBidd/index.tsx
+++ src/components/Bidders/AllBidd/index.tsx
@@ -1,18 +1,28 @@
-export default {
-  data() {
-    return {
-      name : 'testname'
-    }
-  },
-  methods: {
-    name() : number {
-      return 2;
-    },
-  },
+
+import Component from 'vue-class-component'
+import Vue  from "vue";
+
+@Component
+export default class App extends Vue {
+  message = ''
+  created() {
+    const messages = [
+      'I ❤️ VueJS',
+      'I ❤️ TypeStyle',
+      'I ❤️ one file',
+    ]
+
+    const randomIndex = Math.floor(Math.random() * messages.length)
+    this.message = messages[randomIndex]
+  }
+
+  onMessageChanged(message: string) {
+    this.message = message;
+  }
   render(h) {
     return (
-      <div class="counter-wrapper">
-        <p>{this.name}</p>
+      <div class>
+        <p>{this.message}</p>
       </div>
     )
   }
